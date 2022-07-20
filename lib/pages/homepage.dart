e815b7a99556339d2fd8e6eb0fbd907d23e18d31
leanndropx <leanndrompeixoto1@gmail.com>
2022-07-20 10:12:25 -0300

    Função com cronômetro reverso pronta e testada

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -19,18 +19,37 @@ class _HomepageState extends State<Homepage> {
   TextEditingController tasksController = TextEditingController();
   List<TaskModel> allTasks = [];
   String? errorText;
-  int count = 0;
   late Timer timer;
 
-  @override
+  //timer
+  int seconds = 0;
+  int minutes = 0;
+  int hours = 0;
+  bool isTimerRunning = false;
+  bool isTimerPaused = false;
+
+  bool get isTenSeconds => seconds >= 10 || secondsReverse >= 10;
+  bool get isTenMinutes => minutes >= 10 || minutesReverse >= 10;
+  bool get isTenHours => hours >= 10 || hoursReverse >= 10;
+
+  //bool get isTimerRunning => timerRunning=true;
+
+  int secondsReverse = 0;
+  int minutesReverse = 2;
+  int hoursReverse = 1;
+  int minutesReversePaused = 0;
+  int secondsReversedPaused = 0;
+  int hoursReversedPaused = 0;
+
   void initState() {
     // TODO: implement initState
     super.initState();
-    taskRepository.getTasksList().then((value) => {
-          setState(() {
-            allTasks = value;
-          })
-        });
+    taskRepository.getTasksList().then((value) =>
+    {
+      setState(() {
+        allTasks = value;
+      })
+    });
   }
 
   @override
@@ -91,15 +110,17 @@ class _HomepageState extends State<Homepage> {
                 SizedBox(height: 8),
                 Flexible(
                     child: ListView(
-                  shrinkWrap: true,
-                  children: [
-                    for (TaskModel eachTask in allTasks)
-                      TaskContainer(
-                          task: eachTask,
-                          DeleteTask: deleteTask,
-                          TimerDialog: TimerDialog)
-                  ],
-                )),
+                      shrinkWrap: true,
+                      children: [
+                        for (TaskModel eachTask in allTasks)
+                          TaskContainer(
+                            task: eachTask,
+                            DeleteTask: deleteTask,
+                            TimerDialog: ShowTimerDialog,
+                            TimerDialogReverse: ShowReverseTimerDialog,
+                          )
+                      ],
+                    )),
                 SizedBox(height: 8),
                 Row(
                   children: [
@@ -157,10 +178,11 @@ class _HomepageState extends State<Homepage> {
   void deleteAllTasks() {
     showDialog(
         context: context,
-        builder: (context) => AlertDialog(
-              title: const Text('Limpar tudos?'),
+        builder: (context) =>
+            AlertDialog(
+              title: const Text('Limpar tudo?'),
               content:
-                  const Text('Tem certeza que deseja limpar todas as tarefas?'),
+              const Text('Tem certeza que deseja limpar todas as tarefas?'),
               backgroundColor: Colors.white,
               actions: [
                 TextButton(
@@ -186,13 +208,78 @@ class _HomepageState extends State<Homepage> {
             ));
   }
 
+
+
+
+  //actions in Timer Dialog
+  void ShowTimerDialog() {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) =>
+            AlertDialog(
+              title: Center(
+                child: Text(
+                  '${isTenHours ? hours : '0${hours}'}:${isTenMinutes
+                      ? minutes
+                      : '0${minutes}'}:${isTenSeconds
+                      ? seconds
+                      : '0${seconds}'}',
+                  style:
+                  const TextStyle(fontSize: 40, color: Color(0xff00d7f3)),
+                ),
+              ),
+              insetPadding: EdgeInsets.all(12),
+              backgroundColor: Colors.black.withAlpha(800),
+              actions: [
+                TextButton(
+                  onPressed: QuitTimerDialogAndResetTimer,
+                  child: const Text(
+                    'Fechar',
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
+                ),
+                TextButton(
+                    onPressed: ResetTimer,
+                    child: const Text(
+                      'Zerar',
+                      style: TextStyle(color: Color(0xff00d7f3)),
+                    )),
+                TextButton(
+                    onPressed: StopTimer,
+                    child: const Text(
+                      'Parar',
+                      style: TextStyle(color: Color(0xff00d7f3)),
+                    )),
+                TextButton(
+                    onPressed: isTimerRunning ? null : StartTimer,
+                    child: const Text(
+                      'Iniciar',
+                      style: TextStyle(color: Colors.greenAccent),
+                    ))
+              ],
+            ));
+  }
+
   void StartTimer() {
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
-        if (count >= 0) {
-          count++;
+        isTimerRunning = true;
+        if (seconds >= 0) {
+          if (seconds == 59 && minutes == 59) {
+            hours++;
+            minutes = 0;
+            seconds = -1;
+          }
+
+          if (seconds == 59) {
+            minutes++;
+            seconds = -1;
+          }
+
+          seconds++;
           Navigator.of(context).pop();
-          TimerDialog();
+          ShowTimerDialog();
         } else {
           timer.cancel();
         }
@@ -200,43 +287,168 @@ class _HomepageState extends State<Homepage> {
     });
   }
 
-  void TimerDialog() {
-    Navigator.of(context);
+  void ResetTimer() {
+    setState(() {
+      isTimerRunning = false;
+      timer.cancel();
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+      Navigator.pop(context);
+      ShowTimerDialog();
+    });
+  }
+
+  void StopTimer() {
+    setState(() {
+      timer.cancel();
+      isTimerRunning = false;
+      Navigator.of(context).pop();
+      ShowTimerDialog();
+    });
+  }
+
+  void QuitTimerDialogAndResetTimer() {
+    setState(() {
+      isTimerRunning = false;
+      timer.cancel();
+      Navigator.of(context).pop();
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+    });
+  }
+
+
+
+
+  //Actions in Reverse Timer Dialog
+
+  void ShowReverseTimerDialog() {
     showDialog(
         barrierDismissible: false,
         context: context,
-        builder: (context) => AlertDialog(
-              title: Text(
-                '${count}',
-                style: const TextStyle(
-                  fontSize: 100,
-                  color: Color(0xff00d7f3)
+        builder: (context) =>
+            AlertDialog(
+              title: Center(
+                child: Text(
+                  '${isTenHours
+                      ? hoursReverse
+                      : '0${hoursReverse}'}:${isTenMinutes
+                      ? minutesReverse
+                      : '0${minutesReverse}'}:${isTenSeconds
+                      ? secondsReverse
+                      : '0${secondsReverse}'}',
+                  style:
+                  const TextStyle(fontSize: 40, color: Color(0xff00d7f3)),
                 ),
               ),
+              insetPadding: EdgeInsets.all(12),
               backgroundColor: Colors.black.withAlpha(800),
               actions: [
                 TextButton(
-                  onPressed: () {
-                    timer.cancel();
-                    count = 0;
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Fechar'),
+                  onPressed: QuitReverseTimerDialogAndResetTimer,
+                  child: const Text(
+                    'Fechar',
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
                 ),
                 TextButton(
-                    onPressed: () {
-                      setState(() {
-                        timer.cancel();
-                        //Navigator.of(context).pop();
-                      });
-                    },
-                    child: const Text('Parar')),
+                    onPressed: ResetReverseTimer,
+                    child: const Text(
+                      'Zerar',
+                      style: TextStyle(color: Color(0xff00d7f3)),
+                    )),
                 TextButton(
-                    onPressed: () {
-                      StartTimer();
-                    },
-                    child: const Text('Iniciar'))
+                    onPressed: StopReverseTimer,
+                    child: const Text(
+                      'Parar',
+                      style: TextStyle(color: Color(0xff00d7f3)),
+                    )),
+                TextButton(
+                    onPressed: isTimerRunning ? null : StartReverseTimer,
+                    child: const Text(
+                      'Iniciar',
+                      style: TextStyle(color: Colors.greenAccent),
+                    ))
               ],
             ));
+
   }
+
+  void StartReverseTimer() {
+    if (isTimerPaused){
+      secondsReverse = secondsReversedPaused;
+      minutesReverse = minutesReversePaused;
+      hoursReverse = hoursReversedPaused;
+    } else{
+      secondsReverse=60;
+      minutesReverse--;
+    }
+
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        isTimerRunning = true;
+        if(hoursReverse==0 && minutesReverse==0 && secondsReverse==0){
+          timer.cancel();
+        } else {
+          secondsReverse--;
+          if(secondsReverse==-1){
+            if(minutesReverse==0){
+              hoursReverse--;
+              minutesReverse=60;
+              secondsReverse=59;
+            }
+            secondsReverse=59;
+            minutesReverse--;
+          }
+        }
+        Navigator.pop(context);
+        ShowReverseTimerDialog();
+      });
+    });
+  }
+
+  void ResetReverseTimer() {
+    setState(() {
+      isTimerRunning = false;
+      timer.cancel();
+      secondsReverse = 0;
+      minutesReverse = 2;
+      hoursReverse = 1;
+      secondsReversedPaused = secondsReverse;
+      minutesReversePaused = minutesReverse;
+      hoursReversedPaused = hoursReverse;
+      Navigator.pop(context);
+      ShowReverseTimerDialog();
+    });
+  }
+
+  void StopReverseTimer() {
+    setState(() {
+      isTimerPaused = true;
+      minutesReversePaused = minutesReverse;
+      secondsReversedPaused = secondsReverse;
+      hoursReversedPaused = hoursReverse;
+      timer.cancel();
+      isTimerRunning = false;
+      Navigator.of(context).pop();
+      ShowReverseTimerDialog();
+    });
+  }
+
+  void QuitReverseTimerDialogAndResetTimer() {
+    setState(() {
+      isTimerRunning = false;
+      timer.cancel();
+      Navigator.of(context).pop();
+      secondsReverse = 0;
+      minutesReverse = 2;
+      hoursReverse = 1;
+      secondsReversedPaused = secondsReverse;
+      minutesReversePaused = minutesReverse;
+      hoursReversedPaused = hoursReverse;
+    });
+  }
+
 }
--- lib/widgets/taskContainer.dart
+++ lib/widgets/taskContainer.dart
@@ -6,52 +6,68 @@ import 'package:intl/intl.dart';
 import '../models/taskModel.dart';
 
 class TaskContainer extends StatelessWidget {
-  TaskContainer({Key? key, required this.task, required this.DeleteTask, required this.TimerDialog}) : super(key: key);
+  TaskContainer({
+    Key? key,
+    required this.task,
+    required this.DeleteTask,
+    required this.TimerDialog,
+    required this.TimerDialogReverse,
+  }) : super(key: key);
 
   TaskModel task;
   Function(TaskModel) DeleteTask;
-  //Function() StartTimer;
   Function() TimerDialog;
+  Function() TimerDialogReverse;
+
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(child: Container(
-      padding: EdgeInsets.all(16),
-      margin: EdgeInsets.symmetric(vertical: 2),
-      decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(8)
+    return Slidable(
+      child: Container(
+        padding: EdgeInsets.all(16),
+        margin: EdgeInsets.symmetric(vertical: 2),
+        decoration: BoxDecoration(
+            color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MMM/yyyy - EE - HH:mm').format(task.datetime),
+              style: TextStyle(fontSize: 9),
+            ),
+            Text(
+              task.title,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            )
+          ],
+        ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(DateFormat('dd/MMM/yyyy - EE - HH:mm').format(task.datetime),style: TextStyle(
-              fontSize: 9
-          ),),
-          Text(task.title,style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500
-          ),)
-        ],
-      ),
-    ),
       actionExtentRatio: 0.27,
-      actionPane: SlidableDrawerActionPane(), secondaryActions: [
-          IconSlideAction(
-            icon: Icons.delete,
-            color: Colors.red,
-            caption: 'Deletar',
-            onTap: (){
-              DeleteTask(task);
-            },
-          ),
+      actionPane: SlidableDrawerActionPane(),
+      secondaryActions: [
+        IconSlideAction(
+          icon: Icons.delete,
+          color: Colors.red,
+          caption: 'Deletar',
+          onTap: () {
+            DeleteTask(task);
+          },
+        ),
         IconSlideAction(
           icon: Icons.chat,
           color: Colors.green,
           caption: 'Iniciar',
           onTap: TimerDialog,
         )
-      ],);
+      ],
+      actions: [
+        IconSlideAction(
+          icon: Icons.chat,
+          color: Colors.green,
+          caption: 'Temporizador',
+          onTap: TimerDialogReverse,
+        ),
+      ],
+    );
   }
 }
-
